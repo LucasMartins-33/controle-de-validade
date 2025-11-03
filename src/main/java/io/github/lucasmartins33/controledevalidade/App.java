a79cc9538448f8f9ed218e2d45066182a6430c43
Lucas Martins <lucasmartins12369@gmail.com>
2025-11-03 16:06:26 -0300

    estrutra do arquivo app.java montada

--- src/main/java/io/github/lucasmartins33/controledevalidade/App.java
+++ src/main/java/io/github/lucasmartins33/controledevalidade/App.java
@@ -1,4 +1,24 @@
 package io.github.lucasmartins33.controledevalidade;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/TelaPrincipal.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Controle de Validade de Produtos");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
